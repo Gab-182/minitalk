8b28ab91ee29616ecb19469aeb027a1f27caf8c9
Ghaith Abdoush <ghaith181994@hotmail.com>
2022-01-24 12:01:59 +0400

    norm minitalk.h

--- minitalk.h
+++ minitalk.h
@@ -9,17 +9,10 @@
 # include <sys/wait.h>
 # include <sys/types.h>
 
-// typedef struct s_msg
-// {
-//     char    *bin;
-// 	char	letter;
-// 	int     letter_size;
-// }message_list;
-
 size_t	ft_strlen(char *s);
 char	*ft_strjoin(char *s1, char s2);
 void	bin_to_char(char *bin);
-int     ft_atoi(const char *str);
+int		ft_atoi(const char *str);
 void	ft_putchar(int c);
 void	ft_putstr(char *str);
 void	ft_putnbr(int num);
@@ -27,6 +20,6 @@ void	red(void);
 void	green(void);
 void	yellow(void);
 void	reset(void);
-void    error();
+void	error(void);
 
 #endif
